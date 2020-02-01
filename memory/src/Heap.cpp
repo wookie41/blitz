f2a5a00f3daf0a9fb2b14affae7e5530bda3ee51
Łukasz Bogaczyński <woookie41@gmail.com>
2020-02-01 17:55:12 +0100

    Basic allocators (#32)
    Ground work for custom allocators

---
+++ memory/src/Heap.cpp
@@ -0,0 +1,71 @@
+#include <blitzmemory/Heap.h>
+
+static blitz::memory::Heap* heap;
+
+void* operator new(std::size_t count) { return heap->allocate(count); }
+
+void* operator new[](std::size_t count) { return heap->allocate(count); }
+
+void operator delete(void* ptr) { heap->deallocate(ptr); }
+
+void operator delete[](void* ptr) { heap->deallocate(ptr); }
+
+namespace blitz::memory
+{
+    void InitMemory()
+    {
+	    heap = (Heap*)malloc(sizeof(Heap));
+    	heap->Init();
+    }
+
+	void Heap::Init() {}
+    void Heap::Shutdown() {}
+
+    void* Heap::allocate(const size_t& amount)
+    {
+#ifdef USE_LIBC_MALLOC
+        return malloc(amount);
+#endif
+
+        assert(0); // TODO
+    }
+    void* Heap::allocate8(const size_t& amount)
+    {
+#ifdef USE_LIBC_MALLOC
+        return malloc(amount);
+#endif
+
+        assert(0); // TODO
+    }
+
+    void* Heap::allocate16(const size_t& amount)
+    {
+#ifdef USE_LIBC_MALLOC
+        return malloc(amount);
+#endif
+
+        assert(0); // TODO
+    }
+    void* Heap::allocate32(const size_t& amount)
+    {
+#ifdef USE_LIBC_MALLOC
+        return malloc(amount);
+#endif
+        assert(0); // TODO
+    }
+    void* Heap::allocate64(const size_t& amount)
+    {
+#ifdef USE_LIBC_MALLOC
+        return malloc(amount);
+#endif
+        assert(0); // TODO
+    }
+
+    void Heap::Heap::deallocate(void* ptr)
+    {
+#ifdef USE_LIBC_MALLOC
+        free(ptr);
+#endif
+        assert(0); // TODO
+    }
+} // namespace blitz::memory
