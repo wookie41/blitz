203caae23db85256002da7c18fbea3cdd72410f2
Łukasz Bogaczyński <woookie41@gmail.com>
2020-02-01 14:35:38 +0100

    Add custom heap (dummy implementation)

---
+++ memory/include/blitzmemory/Precompiled.h
@@ -0,0 +1,8 @@
+#ifndef BLITZ_MEMORY_PRECOMPILED
+#define BLITZ_MEMORY_PRECOMPILED
+#define _GNU_SOURCE
+#include <cstdlib>
+#include <blitzcommon/DataType.h>
+#include <blitzcommon/NonCopyable.h>
+#include <cassert>
+#endif // BLITZ_MEMORY_PRECOMPILED
